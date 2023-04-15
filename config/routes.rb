6d82c6cb9c8e8a2833f0923c1b161c7100ce90fa
riiiiin <risam1868@gmail.com>
2023-04-14 18:52:50 -0700

    first commit

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  get 'mypage', to: 'users#me'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+  root 'home#index'
+
+  resources :users, only: %i[new create]
+  resources :boards
+  resources :comments, only: %i[create destroy]
+end
